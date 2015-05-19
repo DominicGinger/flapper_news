5376d2fff561af2d56d3ad7921f31a7895315e3a
soup-robot <domginger@hotmail.co.uk>
2015-05-19 22:33:00 +0100

    wip

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: '/home.html',
+          controller: 'MainCtrl'
+        });
+
+      $urlRouterProvider.otherwise('home');
+    }]);
+
+app.factory('posts', [function() {
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
 
 app.controller('MainCtrl', [
-'$scope',
-function($scope){
+'$scope', 'posts',
+function($scope, posts){
   $scope.test = 'Hello World';
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+  $scope.posts = posts.posts;
   $scope.addPost = function(){
-    $scope.posts.push({title: 'A new post!', upvotes: 0});
+    if(!$scope.title || $scope.title === '') { return;}
+    $scope.posts.push({
+      title: $scope.title, 
+      upvotes: 0,
+      link: $scope.link
+    });
+    $scope.title = '';
+    $scope.link = '';
+  };
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
   };
 }]);
